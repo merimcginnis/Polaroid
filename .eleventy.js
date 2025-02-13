44db609e5f99519a27dffa98b89b539b11752249
merimcginnis <166745413+merimcginnis@users.noreply.github.com>
2025-02-13 16:41:25 +0000

    testing on new repo

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
-
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("src/js");
+  
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
 };
