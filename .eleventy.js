c2e4eb115a15f37cd7e870be228ce3e2e4fa68be
merimcginnis <166745413+merimcginnis@users.noreply.github.com>
2025-02-13 17:19:43 +0000

    getting rid of error

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,3 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("src/js");
-  
-  return {
-    dir: {
-      input: ".",
-      output: "_site"
-    }
-  };
-};3
